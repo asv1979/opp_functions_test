3d2a546932ae63baeef5de426bab11ce479dad34
Alexey Vasilyev home <asv2108@gmail.com>
2019-12-30 14:48:34 +0200

    Rewrite my recursive function

--- src/Functions/function.php
+++ src/Functions/function.php
@@ -47,43 +47,24 @@ function getEven($arr)
 /**
  * Get total value from a nested array
  *
- * @param array $arr
+ * @param array $array
  *
  * @return int|mixed
  */
-function getRecursiveArraySum($arr)
-{
-    //TODO use loop while? and need to know a depth of array
-    $sum = 0;
-    foreach ($arr as $item) {
-        if (is_array($item)) {
-            foreach ($item as $item2) {
-                if (is_array($item2)) {
-                    foreach ($item2 as $item3) {
-                        if (is_array($item3)) {
-                            foreach ($item3 as $item4) {
-                                if (is_array($item4)) {
-                                    foreach ($item4 as $item5) {
-                                        if (is_array($item5)) {
-                                            continue;
-                                        } else {
-                                            $sum += $item5;
-                                        }
-                                    }
-                                } else {
-                                    $sum += $item4;
-                                }
-                            }
-                        } else {
-                            $sum += $item3;
-                        }
-                    }
-                } else {
-                    $sum += $item2;
-                }
+function getRecursiveArraySum($array){
+    $sum = NULL;
+    if(is_array($array)){
+        foreach($array as $value){
+            if(is_array($value)){
+                $sum += getRecursiveArraySum($value);
             }
-        } else {
-            $sum += $item;
+            if(is_numeric($value)){
+                $sum += $value;
+            }
+        }
+    }else{
+        if(is_numeric($array)){
+            $sum += $array;
         }
     }
     return $sum;
